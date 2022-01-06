b8da15609af22e3f35d80ae0ecf9347d45a270bf
jessc1 <mmariajessica@gmail.com>
2022-01-06 17:46:18 -0300

    add login

--- latlongproject/settings.py
+++ latlongproject/settings.py
@@ -79,8 +79,8 @@ WSGI_APPLICATION = 'latlongproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'local',
-        'USER': 'postgres',
+        'NAME': '',
+        'USER': '',
         'PASSWORD': '',
     },
 }
@@ -126,4 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
